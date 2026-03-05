3a1f22302ff43f5072b54e2d9d40504a5135cf68
GMor6317 <A01803172@tec.mx>
2026-03-04 21:06:57 -0600

    Background, mapa y movimientos básicos
    Se agregaron todos los elementos necesario para formar el mapa y se ajustaron los movimientos básicos al personaje

---
+++ Assets/Scripts/MoverConInputActions.cs
@@ -0,0 +1,56 @@
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoverConInputActions : MonoBehaviour
+{
+    [SerializeField]
+    private InputAction accionMover; //en las 4 posiciones
+
+    [SerializeField]
+    private InputAction accionSaltar; //Saltar con el espacio
+
+    private float velocidadX = 7f; //Definiendo la velocidad maxima que tendra en el eje X
+    private float velocidadY = 12f;
+
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Habilitar el InputAction
+        accionMover.Enable();
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+
+    void OnEnable() //Si el objeto esta habilitado se pueda utilizar, si no no
+    {
+        accionSaltar.Enable();
+        accionSaltar.performed += saltar; //Se le dice al objeto que agregue en su lista de funciones la funcion saltar      
+    }
+
+    void OnDisable() //Si el objeto esta deshabilitado no se va a poder utilizar
+    {
+        accionSaltar.Disable();
+        accionSaltar.performed -= saltar; //Se le dice al objeto que quite de su lista de funciones la funcion saltar      
+        
+    }
+
+    public void saltar(InputAction.CallbackContext context) //Debe contener el parametro, si no no se reconoce
+    {
+        //Rigidbody2D rb = GetComponent<Rigidbody2D>(); //Se define rigidbody de manera local para poder hacer un salto
+        rb.linearVelocityY = velocidadY * 1; //Se cambia la velocidad en Y
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Leer la entrada
+        Vector2 movimiento = accionMover.ReadValue<Vector2>(); //Declaramos un vector que lea los valores del movimiento, es un Vector2 ya que solo tenemos x y
+        //transform.position = (Vector2)transform.position + Time.deltaTime * movimiento * velocidadX;
+        rb.linearVelocityX = velocidadX * movimiento.x;
+    }
+}
